cb714a2c4923cc6187f09d35e06e3c4f53e928e4
Harshad <hbirajdar1@gmail.com>
2022-12-24 12:54:48 +0530

    config file added

---
+++ config/index.ts
@@ -0,0 +1,6 @@
+
+const config = () => ({
+    PORT : process.env.PORT || 4000,
+  });
+
+export default config
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import config from 'config';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('/v1/api');
+  await app.listen(config().PORT);
 }
 bootstrap();
